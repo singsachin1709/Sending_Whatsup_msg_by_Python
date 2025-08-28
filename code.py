9bc028759cd0df1fca463e32a1558bb32607ff1b
Sachin Singh <114739993+singsachin1709@users.noreply.github.com>
2025-08-29 00:47:19 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+import pywhatkit as kit
+
+# Send WhatsApp message instantly
+kit.sendwhatmsg_instantly(
+    phone_no="+919876543210",    
+    message="Hello 👋 This is an automated WhatsApp message using Python!"
+)
